2c3fb18cc2cad8b628b667d041de73bacf5ff868
ashesh790 <asheshtparmar@gmail.com>
2023-02-19 21:42:41 +0530

    Signup system created

--- user_1/models.py
+++ user_1/models.py
@@ -1,9 +1,10 @@
+import uuid
 from django.db import models
 
 # Create your models here. 
 
 class User_register(models.Model): 
-    user_id=models.CharField(primary_key=True, max_length=11, unique=True)  
+    user_id=models.SlugField(primary_key=True, max_length=11, unique=True, auto_created=True, default=uuid.uuid1)  
     user_name=models.CharField(max_length=20)
     user_email=models.CharField(max_length=25)
     user_mobile=models.CharField(max_length=20, null=True)
@@ -12,33 +13,5 @@ class User_register(models.Model):
 
 # Using right now 
 class p_detail(models.Model): 
-    seller_id=models.ForeignKey(User_register, on_delete=models.CASCADE, default=None) 
+    seller_id=models.ForeignKey(User_register, on_delete=models.CASCADE)  
     property_data=models.JSONField() 
-    
-# Using not right now 
-class Property_detail(models.Model): 
-    seller_id=models.ForeignKey(User_register, on_delete=models.CASCADE, default=None)  
-    property_id=models.IntegerField(primary_key=True)
-    property_type=models.CharField(max_length=150) 
-    property_age=models.IntegerField()
-    selling_option=models.CharField(max_length=150) 
-    construction_status=models.CharField(max_length=150) 
-    floor=models.IntegerField()
-    bhk=models.IntegerField()
-    bathroom=models.IntegerField()
-    balcony=models.IntegerField()
-    furnish_type=models.CharField(max_length=150)
-    geography_area=models.CharField(max_length=150)
-    parking_type=models.CharField(max_length=150)
-    property_value=models.CharField(max_length=150)
-    property_rent_price=models.CharField(max_length=150)
-    from_avail_property_date=models.DateField()
-    property_address=models.CharField(max_length=150)
-
-class Property_other_detail(models.Model): 
-    # Property media like image and video or many other detail will be here.
-    media_id=models.ForeignKey(p_detail, on_delete=models.CASCADE) 
-    property_image_1=models.FileField(upload_to="media/") 
-    property_image_2=models.FileField(upload_to="media/") 
-    property_image_3=models.FileField(upload_to="media/") 
-
