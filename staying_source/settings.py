6e6c813a2f5d0bfc83790a5538b7abe61a5569e9
Ashesh009 <ashesh@ouiteo.com>
2023-10-21 13:17:04 +0530

    Google login v.1

--- staying_source/settings.py
+++ staying_source/settings.py
@@ -161,6 +161,6 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend", 
     "allauth.account.auth_backends.AuthenticationBackend"
 )
-
+SOCIALACCOUNT_LOGIN_ON_GET=True
 LOGIN_REDIRECT_URL = "/" 
 LOGOUT_REDIRECT_URL = "/" 
