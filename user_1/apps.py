a38862c88cf82c52f43300fddfb8684e8735ef6a
Ashesh009 <ashesh@ouiteo.com>
2024-03-10 11:58:14 +0530

    UI Updated.

--- user_1/apps.py
+++ user_1/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class User1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_1'
+
+# Token: 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Token
